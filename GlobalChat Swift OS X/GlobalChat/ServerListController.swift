fdb63d835ba19dd79a8877ac596f80b8950cb239
Jonathan Silverman <jonathansilverman@Jonathans-MBP-2.lan>
2020-07-03 14:24:03 -0600

    hack highlighting problem

--- GlobalChat Swift OS X/GlobalChat/ServerListController.swift
+++ GlobalChat Swift OS X/GlobalChat/ServerListController.swift
@@ -19,7 +19,7 @@ class ServerListController: NSViewController, NSTableViewDataSource {
     @IBOutlet weak var password: NSTextField!
     @IBOutlet weak var server_list_table: NSTableView!
     
-    var names: [String]?
+    var names: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,11 +28,28 @@ class ServerListController: NSViewController, NSTableViewDataSource {
     }
     
     func get_servers() {
+        self.names = []
+        
         let url = URL(string: "https://nexus-msl.herokuapp.com/msl")!
 
         let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
             guard let data = data else { return }
             print(String(data: data, encoding: .utf8)!)
+            let data_string = String(data: data, encoding: .utf8)
+            let each_line = data_string!.split(separator: Character("\n"))
+            for server in each_line {
+                let server_info = server.components(separatedBy: "-!!!-")
+                let name = server_info[0]
+                let ip = server_info[1]
+                let port = server_info[2]
+                self.names.append(String(name))
+            }
+            
+            
+            DispatchQueue.main.async {
+                self.server_list_table.reloadData()
+            }
+            
         }
 
         task.resume()
@@ -51,19 +68,12 @@ class ServerListController: NSViewController, NSTableViewDataSource {
     }
 
     func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
-        if names != nil {
-            return names![row]
-        } else {
-            return 0
-        }
+        print(self.names)
+        return self.names[row]
     }
 
     func numberOfRows(in: NSTableView) -> Int {
-        if names == nil {
-            return 0
-        } else {
-            return names!.count
-        }
+        return self.names.count
     }
     
 }
