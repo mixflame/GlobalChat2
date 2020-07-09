be833d2655c9ee1d26fc0c6fcce223f08055874a
Jonathan Silverman <jonathansilverman@Jonathans-MBP-2.lan>
2020-07-08 19:39:44 -0600

    add Kotlin android app (blank)

---
+++ GlobalChat-Kotlin-Android/app/src/main/java/com/lookrative/globalchat2androidkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lookrative.globalchat2androidkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
