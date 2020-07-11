fd502b14c3a551c7e73988d9fe3cc498ed82d148
Jonathan Silverman <jonathansilverman@Jonathans-MBP-2.lan>
2020-07-11 15:06:17 -0600

    correct addClick function and correct send point

--- GlobalChat Swift OS X/GlobalChat/GlobalDrawController.swift
+++ GlobalChat Swift OS X/GlobalChat/GlobalDrawController.swift
@@ -12,42 +12,145 @@ class GlobalDrawController: NSViewController {
     
     @IBOutlet weak var drawing_view: NSView!
     
-    
     var gcc: GlobalChatController?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+//        print("viewDidLoad: gdc")
     }
     
 }
 
 class LineDrawer : NSView {
-  var newLinear = NSBezierPath()
+    var newLinear = NSBezierPath()
+    
+    
+    var points : [[String : Any]] = []
+    
+    var nameHash : [String : Int] = [:] // which layer is this handle on
+    
+    var layerOrder : [String] = [] // which order to draw layers
+    
+    var layers : [String : Any] = [:] // which points are in a layer
+    
+//    var username : String = ""
+    
+    var scribbling : Bool = false
+    var pen_color : NSColor = NSColor.black.usingColorSpace(NSColorSpace.deviceRGB)!
+    var pen_width : CGFloat = CGFloat(1)
+    
+    // void addClick(int x, int y, bool dragging, int r, int g, int b, int width, QString clickName);
+    func addClick(_ x: CGFloat, y: CGFloat, dragging: Bool, red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat, width: CGFloat, clickName: String) {
+        var point : [String : Any] = [:]
+        point["x"] = x
+        point["y"] = y
+        point["dragging"] = dragging
+        point["red"] = red
+        point["green"] = green
+        point["blue"] = blue
+        point["alpha"] = alpha
+        point["width"] = width
+        point["clickName"] = clickName
+        points.append(point)
+        
+        var layerName : String = ""
+        
+        if(nameHash[clickName] == nil) {
+            let layer = 0
+            nameHash[clickName] = layer
+            layerName = "\(clickName)_\(layer)"
+            let layerArray : [[String : Any]] = []
+            layers[layerName] = layerArray
+        } else {
+            if(dragging == false) {
+                let layer = nameHash[clickName]! + 1
+                nameHash[clickName] = layer
+                layerName = "\(clickName)_\(layer)"
+                let layerArray : [[String : Any]] = []
+                layers[layerName] = layerArray
+            } else {
+                let layer = nameHash[clickName]!
+                layerName = "\(clickName)_\(layer)"
+            }
+        }
+        
+        var tempLayers = layers[layerName] as! [[String : Any]]
+        tempLayers.append(point)
+        layers[layerName] = tempLayers
+        
+        if(!layerOrder.contains(layerName)) {
+            layerOrder.append(layerName)
+        }
+        
+        print(layerOrder)
+        
+    }
+    
+    func redraw() {
+        
+    }
 
     override func draw(_ dirtyRect: NSRect) {
-        NSColor.red.set()
-        newLinear.lineWidth = 1
-        newLinear.stroke()
+//        let gdc = self.window?.contentViewController as! GlobalDrawController
+//        pen_color.set()
+//        newLinear.lineWidth = pen_width
+//        newLinear.stroke()
+        redraw()
     }
 
     override func mouseDown(with event: NSEvent) {
+        let gdc = self.window?.contentViewController as! GlobalDrawController
         super.mouseDown(with: event)
-//        let location = event.locationInWindow
-//        var lastPt = event.locationInWindow
+        scribbling = true
+        
         var lastPt = convert(event.locationInWindow, from: nil)
         lastPt.x -= frame.origin.x
         lastPt.y -= frame.origin.y
-        newLinear.move(to: lastPt)
+        
+        addClick(lastPt.x, y: lastPt.y, dragging: false, red: pen_color.redComponent, green: pen_color.greenComponent, blue: pen_color.blueComponent, alpha: pen_color.alphaComponent, width: pen_width, clickName: gdc.gcc!.handle)
+        
+        send_point(lastPt.x, y: lastPt.y, dragging: false, red: pen_color.redComponent, green: pen_color.greenComponent, blue: pen_color.blueComponent, alpha: pen_color.alphaComponent, width: pen_width, clickName: gdc.gcc!.handle)
+        
+//        newLinear.move(to: lastPt)
     }
 
     override func mouseDragged(with event: NSEvent) {
+        let gdc = self.window?.contentViewController as! GlobalDrawController
         super.mouseDragged(with: event)
 //        var newPt = event.locationInWindow
         var newPt = convert(event.locationInWindow, from: nil)
         newPt.x -= frame.origin.x
         newPt.y -= frame.origin.y
-        newLinear.line(to: newPt)
+        
+        addClick(newPt.x, y: newPt.y, dragging: true, red: pen_color.redComponent, green: pen_color.greenComponent, blue: pen_color.blueComponent, alpha: pen_color.alphaComponent, width: pen_width, clickName: gdc.gcc!.handle)
+        
+        
+        send_point(newPt.x, y: newPt.y, dragging: true, red: pen_color.redComponent, green: pen_color.greenComponent, blue: pen_color.blueComponent, alpha: pen_color.alphaComponent, width: pen_width, clickName: gdc.gcc!.handle)
+//        newLinear.line(to: newPt)
         needsDisplay = true
     }
+    
+    override func mouseUp(with event: NSEvent) {
+        super.mouseUp(with: event)
+        scribbling = false
+    }
+    
+    
+    func send_point(_ x: CGFloat, y: CGFloat, dragging: Bool, red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat, width: CGFloat, clickName: String) {
+        let gdc = self.window?.contentViewController as! GlobalDrawController
+        var point : [String] = []
+        point.append(String(x.description))
+        point.append(String(y.description))
+        point.append(String(dragging.description))
+        point.append(String(red.description))
+        point.append(String(green.description))
+        point.append(String(blue.description))
+        point.append(String(alpha.description))
+        point.append(String(width.description))
+        point.append(String(clickName))
+        gdc.gcc?.send_message("POINT", args: point)
+    }
+    
 }
